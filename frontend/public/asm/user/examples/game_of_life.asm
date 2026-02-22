bf84dacf122f4d5fd3b41f05e213c1a2f7e9fca6
Max YO <max@monstermax.net>
2026-02-22 05:43:06 +0100

    dev 2026-02

---
+++ frontend/public/asm/user/examples/game_of_life.asm
@@ -0,0 +1,637 @@
+; Author: Bob + yomax
+; Name: game_of_life
+; Description: Conway's Game of Life (16x16, rendered 2x2 on 32x32)
+;
+; Controls:
+; - N / SPACE: next generation
+; - C: clear
+; - R: random fill
+; - ESC: quit
+; - Switch 0: next generation
+; - Switch 1: clear
+; - Switch 2: random fill
+
+.include "os/v3/drivers/lib_screen.asm"
+.include "os/v3/drivers/lib_keyboard.asm"
+.include "os/v3/drivers/lib_console.asm"
+.include "os/v3/drivers/lib_switchs.asm"
+
+section .data
+    KEY_N         equ 110
+    KEY_N_UPPER   equ 78
+    KEY_SPACE     equ 32
+    KEY_C         equ 99
+    KEY_C_UPPER   equ 67
+    KEY_R         equ 114
+    KEY_R_UPPER   equ 82
+    KEY_ESC       equ 27
+
+    COL_BG        equ 0x00
+    COL_CELL      equ 0x55
+
+    quit_flag     db 0
+
+    rng_io_base   dw 0xF0A0
+
+    msg_help_1    db "LIFE: N/SPACE next, C clear, R random", 0
+    msg_help_2    db "Switch0 next, Switch1 clear, Switch2 random", 0
+    msg_eol       db 10, 0
+
+    ; 16x16 = 256 bytes
+    cur_grid:
+        ; first 16 bytes row0
+        db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+        db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+        db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+        db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+        db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+        db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+        db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+        db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+        db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+        db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+        db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+        db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+        db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+        db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+        db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+        db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+
+    next_grid:
+        db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+        db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+        db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+        db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+        db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+        db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+        db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+        db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+        db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+        db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+        db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+        db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+        db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+        db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+        db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+        db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+
+section .text
+    global _start
+
+_start:
+    mov al, 0
+    mov [quit_flag], al
+
+    call gol_seed_glider
+    call gol_render
+    call gol_print_help
+
+gol_main:
+    call gol_handle_switches
+    call gol_handle_keyboard
+
+    mov al, [quit_flag]
+    cmp al, 1
+    je gol_exit
+
+    jmp gol_main
+
+gol_exit:
+    ret
+
+gol_print_help:
+    push cl
+    push dl
+    lea cl, dl, [msg_help_1]
+    call console_print_string
+    lea cl, dl, [msg_eol]
+    call console_print_string
+    lea cl, dl, [msg_help_2]
+    call console_print_string
+    lea cl, dl, [msg_eol]
+    call console_print_string
+    pop dl
+    pop cl
+    ret
+
+gol_handle_switches:
+    push al
+
+    call get_switchs_pending
+    cmp al, 0xFF
+    je gol_sw_done
+
+    cmp al, 0
+    je gol_sw_next
+    cmp al, 1
+    je gol_sw_clear
+    cmp al, 2
+    je gol_sw_random
+    jmp gol_sw_ack
+
+gol_sw_next:
+    call gol_step
+    call gol_render
+    jmp gol_sw_ack
+
+gol_sw_clear:
+    call gol_clear_all
+    call gol_render
+    jmp gol_sw_ack
+
+gol_sw_random:
+    call gol_random_fill
+    call gol_render
+
+gol_sw_ack:
+    ; ack index in A
+    call ack_switchs
+
+gol_sw_done:
+    pop al
+    ret
+
+gol_handle_keyboard:
+    push al
+
+    call get_keyboard_status
+    and al, 0x01
+    cmp al, 0
+    je gol_k_done
+
+    call get_keyboard_char
+    call set_keyboard_status
+
+    cmp al, KEY_ESC
+    je gol_k_esc
+
+    cmp al, KEY_SPACE
+    je gol_k_next
+    cmp al, KEY_N
+    je gol_k_next
+    cmp al, KEY_N_UPPER
+    je gol_k_next
+
+    cmp al, KEY_C
+    je gol_k_clear
+    cmp al, KEY_C_UPPER
+    je gol_k_clear
+
+    cmp al, KEY_R
+    je gol_k_rand
+    cmp al, KEY_R_UPPER
+    je gol_k_rand
+
+    jmp gol_k_done
+
+gol_k_esc:
+    mov al, 1
+    mov [quit_flag], al
+    jmp gol_k_done
+
+gol_k_next:
+    call gol_step
+    call gol_render
+    jmp gol_k_done
+
+gol_k_clear:
+    call gol_clear_all
+    call gol_render
+    jmp gol_k_done
+
+gol_k_rand:
+    call gol_random_fill
+    call gol_render
+
+gol_k_done:
+    pop al
+    ret
+
+gol_seed_glider:
+    call gol_clear_all
+    ; simple glider near center
+    ; (6,5) (7,6) (5,7) (6,7) (7,7)
+    mov fl, 6
+    mov el, 5
+    mov al, 1
+    call gol_set_cur
+
+    mov fl, 7
+    mov el, 6
+    mov al, 1
+    call gol_set_cur
+
+    mov fl, 5
+    mov el, 7
+    mov al, 1
+    call gol_set_cur
+
+    mov fl, 6
+    mov el, 7
+    mov al, 1
+    call gol_set_cur
+
+    mov fl, 7
+    mov el, 7
+    mov al, 1
+    call gol_set_cur
+    ret
+
+gol_clear_all:
+    push al
+    push cl
+    push dl
+    push el
+    push fl
+
+    mov el, 0
+gol_clear_y:
+    cmp el, 16
+    je gol_clear_done
+    mov fl, 0
+gol_clear_x:
+    cmp fl, 16
+    je gol_clear_ny
+    mov al, 0
+    call gol_set_cur
+    call gol_set_next
+    inc fl
+    jmp gol_clear_x
+gol_clear_ny:
+    inc el
+    jmp gol_clear_y
+gol_clear_done:
+    pop fl
+    pop el
+    pop dl
+    pop cl
+    pop al
+    ret
+
+gol_random_fill:
+    push al
+    push cl
+    push dl
+    push el
+    push fl
+
+    mov cl, [rng_io_base]
+    mov dl, [rng_io_base + 1]
+
+    mov el, 0
+gol_rand_y:
+    cmp el, 16
+    je gol_rand_done
+    mov fl, 0
+gol_rand_x:
+    cmp fl, 16
+    je gol_rand_ny
+
+    ldi al, cl, dl
+    and al, 0x01
+    call gol_set_cur
+
+    inc fl
+    jmp gol_rand_x
+gol_rand_ny:
+    inc el
+    jmp gol_rand_y
+gol_rand_done:
+    pop fl
+    pop el
+    pop dl
+    pop cl
+    pop al
+    ret
+
+; compute one generation
+gol_step:
+    push al
+    push bl
+    push cl
+    push dl
+    push el
+    push fl
+
+    mov el, 0
+gol_step_y:
+    cmp el, 16
+    je gol_copy_next
+    mov fl, 0
+gol_step_x:
+    cmp fl, 16
+    je gol_step_ny
+
+    ; neighbor count in BL
+    mov bl, 0
+
+    ; y-1 row
+    cmp el, 0
+    je gol_step_midrow
+    ; x-1,y-1
+    cmp fl, 0
+    je gol_step_n1b
+    dec fl
+    dec el
+    call gol_get_cur
+    add bl, al
+    inc fl
+    inc el
+gol_step_n1b:
+    ; x,y-1
+    dec el
+    call gol_get_cur
+    add bl, al
+    inc el
+    ; x+1,y-1
+    cmp fl, 15
+    je gol_step_midrow
+    inc fl
+    dec el
+    call gol_get_cur
+    add bl, al
+    dec fl
+    inc el
+
+gol_step_midrow:
+    ; x-1,y
+    cmp fl, 0
+    je gol_step_n3b
+    dec fl
+    call gol_get_cur
+    add bl, al
+    inc fl
+gol_step_n3b:
+    ; x+1,y
+    cmp fl, 15
+    je gol_step_botrow
+    inc fl
+    call gol_get_cur
+    add bl, al
+    dec fl
+
+gol_step_botrow:
+    cmp el, 15
+    je gol_step_apply
+
+    ; x-1,y+1
+    cmp fl, 0
+    je gol_step_n6b
+    dec fl
+    inc el
+    call gol_get_cur
+    add bl, al
+    inc fl
+    dec el
+gol_step_n6b:
+    ; x,y+1
+    inc el
+    call gol_get_cur
+    add bl, al
+    dec el
+    ; x+1,y+1
+    cmp fl, 15
+    je gol_step_apply
+    inc fl
+    inc el
+    call gol_get_cur
+    add bl, al
+    dec fl
+    dec el
+
+gol_step_apply:
+    ; current state in AL
+    call gol_get_cur
+    cmp al, 1
+    je gol_step_live
+
+    ; dead -> alive if neighbors==3
+    mov al, 0
+    cmp bl, 3
+    jne gol_step_store
+    mov al, 1
+    jmp gol_step_store
+
+gol_step_live:
+    ; live survives with 2 or 3
+    mov al, 0
+    cmp bl, 2
+    je gol_step_survive
+    cmp bl, 3
+    je gol_step_survive
+    jmp gol_step_store
+gol_step_survive:
+    mov al, 1
+
+gol_step_store:
+    call gol_set_next
+
+    inc fl
+    jmp gol_step_x
+gol_step_ny:
+    inc el
+    jmp gol_step_y
+
+; copy next->cur
+gol_copy_next:
+    mov el, 0
+gol_copy_y:
+    cmp el, 16
+    je gol_step_done
+    mov fl, 0
+gol_copy_x:
+    cmp fl, 16
+    je gol_copy_ny
+    call gol_get_next
+    call gol_set_cur
+    inc fl
+    jmp gol_copy_x
+gol_copy_ny:
+    inc el
+    jmp gol_copy_y
+
+gol_step_done:
+    pop fl
+    pop el
+    pop dl
+    pop cl
+    pop bl
+    pop al
+    ret
+
+gol_render:
+    push al
+    push el
+    push fl
+
+    ; clear 32x32
+    mov al, COL_BG
+    mov el, 0
+gol_ry:
+    cmp el, 32
+    je gol_draw_cells
+    mov fl, 0
+gol_rx:
+    cmp fl, 32
+    je gol_rny
+    call screen_set_pixel
+    inc fl
+    jmp gol_rx
+gol_rny:
+    inc el
+    jmp gol_ry
+
+; each grid cell => 2x2 block
+gol_draw_cells:
+    mov el, 0
+gol_dy:
+    cmp el, 16
+    je gol_render_done
+    mov fl, 0
+gol_dx:
+    cmp fl, 16
+    je gol_dny
+
+    call gol_get_cur
+    cmp al, 1
+    jne gol_dx_next
+
+    mov al, COL_CELL
+    call gol_draw_cell2
+
+gol_dx_next:
+    inc fl
+    jmp gol_dx
+gol_dny:
+    inc el
+    jmp gol_dy
+
+gol_render_done:
+    pop fl
+    pop el
+    pop al
+    ret
+
+; in FL=x(0..15), EL=y(0..15)
+gol_draw_cell2:
+    push al
+    push bl
+    push cl
+
+    ; px = x*2, py = y*2
+    mov bl, fl
+    shl bl, 1
+    mov cl, el
+    shl cl, 1
+
+    mov fl, bl
+    mov el, cl
+    call screen_set_pixel
+
+    inc fl
+    call screen_set_pixel
+
+    dec fl
+    inc el
+    call screen_set_pixel
+
+    inc fl
+    call screen_set_pixel
+
+    pop cl
+    pop bl
+    pop al
+    ret
+
+; grid access helpers
+; in FL=x, EL=y
+; out AL=cur[y*16+x]
+gol_get_cur:
+    push cl
+    push dl
+    push bl
+
+    mov bl, el
+    shl bl, 4
+    add bl, fl
+
+    lea cl, dl, [cur_grid]
+    add cl, bl
+    jnc gol_gc_noc
+    inc dl
+gol_gc_noc:
+    ldi al, cl, dl
+
+    pop bl
+    pop dl
+    pop cl
+    ret
+
+; in FL=x, EL=y, AL=value(0/1)
+gol_set_cur:
+    push cl
+    push dl
+    push bl
+
+    mov bl, al
+    mov al, el
+    shl al, 4
+    add al, fl
+
+    lea cl, dl, [cur_grid]
+    add cl, al
+    jnc gol_sc_noc
+    inc dl
+gol_sc_noc:
+    sti cl, dl, bl
+
+    pop bl
+    pop dl
+    pop cl
+    ret
+
+; in FL=x, EL=y
+; out AL=next[y*16+x]
+gol_get_next:
+    push cl
+    push dl
+    push bl
+
+    mov bl, el
+    shl bl, 4
+    add bl, fl
+
+    lea cl, dl, [next_grid]
+    add cl, bl
+    jnc gol_gn_noc
+    inc dl
+gol_gn_noc:
+    ldi al, cl, dl
+
+    pop bl
+    pop dl
+    pop cl
+    ret
+
+; in FL=x, EL=y, AL=value(0/1)
+gol_set_next:
+    push cl
+    push dl
+    push bl
+
+    mov bl, al
+    mov al, el
+    shl al, 4
+    add al, fl
+
+    lea cl, dl, [next_grid]
+    add cl, al
+    jnc gol_sn_noc
+    inc dl
+gol_sn_noc:
+    sti cl, dl, bl
+
+    pop bl
+    pop dl
+    pop cl
+    ret
---
+++ frontend/public/asm/user/examples/tower_of_hanoi.asm
@@ -0,0 +1,728 @@
+; Author: Bob + yomax
+; Name: tower_of_hanoi
+; Description: Tower of Hanoi (3 pegs, 3 disks) on 32x32 screen
+;
+; Controls:
+; - 1/2/3: select source peg, then destination peg
+; - R: reset
+; - ESC: quit
+
+.include "os/v3/drivers/lib_screen.asm"
+.include "os/v3/drivers/lib_keyboard.asm"
+.include "os/v3/drivers/lib_console.asm"
+
+section .data
+    KEY_1         equ 49
+    KEY_2         equ 50
+    KEY_3         equ 51
+    KEY_R         equ 114
+    KEY_R_UPPER   equ 82
+    KEY_ESC       equ 27
+
+    COL_BG        equ 0x00
+    COL_PEG       equ 0x80
+    COL_D1        equ 0x30
+    COL_D2        equ 0x60
+    COL_D3        equ 0x90
+    COL_WIN       equ 0x55
+
+    selected_src  db 0xFF
+    quit_flag     db 0
+    win_flag      db 0
+
+    peg0_count    db 3
+    peg1_count    db 0
+    peg2_count    db 0
+
+    ; bottom->top slots
+    peg0_0        db 3
+    peg0_1        db 2
+    peg0_2        db 1
+
+    peg1_0        db 0
+    peg1_1        db 0
+    peg1_2        db 0
+
+    peg2_0        db 0
+    peg2_1        db 0
+    peg2_2        db 0
+
+    msg_help_1    db "HANOI: press 1/2/3 then 1/2/3", 0
+    msg_help_2    db "Move all disks to peg 3", 0
+    msg_invalid   db "Invalid move", 0
+    msg_win       db "Solved!", 0
+    msg_eol       db 10, 0
+
+section .text
+    global _start
+
+_start:
+    call hanoi_reset
+    call hanoi_print_help
+
+hanoi_main_loop:
+    call hanoi_handle_input
+
+    mov al, [quit_flag]
+    cmp al, 1
+    je hanoi_quit
+
+    jmp hanoi_main_loop
+
+hanoi_quit:
+    ret
+
+hanoi_reset:
+    mov al, 0
+    mov [quit_flag], al
+    mov [win_flag], al
+    mov al, 0xFF
+    mov [selected_src], al
+
+    mov al, 3
+    mov [peg0_count], al
+    mov al, 0
+    mov [peg1_count], al
+    mov [peg2_count], al
+
+    mov al, 3
+    mov [peg0_0], al
+    mov al, 2
+    mov [peg0_1], al
+    mov al, 1
+    mov [peg0_2], al
+
+    mov al, 0
+    mov [peg1_0], al
+    mov [peg1_1], al
+    mov [peg1_2], al
+    mov [peg2_0], al
+    mov [peg2_1], al
+    mov [peg2_2], al
+
+    call hanoi_render
+    ret
+
+hanoi_print_help:
+    push cl
+    push dl
+    lea cl, dl, [msg_help_1]
+    call console_print_string
+    lea cl, dl, [msg_eol]
+    call console_print_string
+    lea cl, dl, [msg_help_2]
+    call console_print_string
+    lea cl, dl, [msg_eol]
+    call console_print_string
+    pop dl
+    pop cl
+    ret
+
+hanoi_print_invalid:
+    push cl
+    push dl
+    lea cl, dl, [msg_invalid]
+    call console_print_string
+    lea cl, dl, [msg_eol]
+    call console_print_string
+    pop dl
+    pop cl
+    ret
+
+hanoi_print_win:
+    push cl
+    push dl
+    lea cl, dl, [msg_win]
+    call console_print_string
+    lea cl, dl, [msg_eol]
+    call console_print_string
+    pop dl
+    pop cl
+    ret
+
+hanoi_handle_input:
+    push al
+    push bl
+
+    call get_keyboard_status
+    and al, 0x01
+    cmp al, 0
+    je hanoi_input_done
+
+    call get_keyboard_char
+    mov bl, al
+    call set_keyboard_status
+
+    ; reset/quit
+    mov al, bl
+    cmp al, KEY_ESC
+    je hanoi_key_esc
+    cmp al, KEY_R
+    je hanoi_key_reset
+    cmp al, KEY_R_UPPER
+    je hanoi_key_reset
+
+    ; peg key -> AL in [0..2]
+    mov al, bl
+    cmp al, KEY_1
+    je hanoi_key_p1
+    cmp al, KEY_2
+    je hanoi_key_p2
+    cmp al, KEY_3
+    je hanoi_key_p3
+    jmp hanoi_input_done
+
+hanoi_key_esc:
+    mov al, 1
+    mov [quit_flag], al
+    jmp hanoi_input_done
+
+hanoi_key_reset:
+    call hanoi_reset
+    jmp hanoi_input_done
+
+hanoi_key_p1:
+    mov al, 0
+    jmp hanoi_select_or_move
+hanoi_key_p2:
+    mov al, 1
+    jmp hanoi_select_or_move
+hanoi_key_p3:
+    mov al, 2
+
+hanoi_select_or_move:
+    ; AL = peg index pressed
+    cmp [selected_src], 0xFF
+    je hanoi_set_src
+
+    ; destination selected -> attempt move src->dst
+    mov bl, al ; dst
+    mov al, [selected_src] ; src
+    call hanoi_try_move
+
+    mov al, 0xFF
+    mov [selected_src], al
+    call hanoi_render
+    jmp hanoi_input_done
+
+hanoi_set_src:
+    mov [selected_src], al
+
+hanoi_input_done:
+    pop bl
+    pop al
+    ret
+
+; In: AL=src, BL=dst
+hanoi_try_move:
+    push cl
+    push dl
+    push el
+    push fl
+
+    ; src top disk -> CL (0 means empty)
+    call hanoi_get_top
+    mov cl, al
+    cmp cl, 0
+    je hanoi_try_move_invalid
+
+    ; dst top disk -> DL
+    mov al, bl
+    call hanoi_get_top
+    mov dl, al
+
+    ; valid if dst empty OR dst > src (larger number means larger disk)
+    cmp dl, 0
+    je hanoi_try_move_valid
+
+    ; if dst < src => invalid
+    cmp dl, cl
+    jl hanoi_try_move_invalid
+
+hanoi_try_move_valid:
+    ; pop src
+    mov al, [selected_src]
+    call hanoi_pop
+
+    ; push to dst (disk in CL)
+    mov al, bl
+    mov fl, cl
+    call hanoi_push
+
+    ; win if peg2_count == 3
+    mov al, [peg2_count]
+    cmp al, 3
+    jne hanoi_try_move_end
+    mov al, 1
+    mov [win_flag], al
+    call hanoi_print_win
+    jmp hanoi_try_move_end
+
+hanoi_try_move_invalid:
+    call hanoi_print_invalid
+
+hanoi_try_move_end:
+    pop fl
+    pop el
+    pop dl
+    pop cl
+    ret
+
+; In AL=peg idx, Out AL=top disk (0 if empty)
+hanoi_get_top:
+    push bl
+    mov bl, al
+
+    cmp bl, 0
+    je hanoi_top_p0
+    cmp bl, 1
+    je hanoi_top_p1
+    ; p2
+    mov al, [peg2_count]
+    cmp al, 0
+    je hanoi_top_empty
+    cmp al, 1
+    je hanoi_top_p2_0
+    cmp al, 2
+    je hanoi_top_p2_1
+    mov al, [peg2_2]
+    jmp hanoi_top_done
+hanoi_top_p2_0:
+    mov al, [peg2_0]
+    jmp hanoi_top_done
+hanoi_top_p2_1:
+    mov al, [peg2_1]
+    jmp hanoi_top_done
+
+hanoi_top_p0:
+    mov al, [peg0_count]
+    cmp al, 0
+    je hanoi_top_empty
+    cmp al, 1
+    je hanoi_top_p0_0
+    cmp al, 2
+    je hanoi_top_p0_1
+    mov al, [peg0_2]
+    jmp hanoi_top_done
+hanoi_top_p0_0:
+    mov al, [peg0_0]
+    jmp hanoi_top_done
+hanoi_top_p0_1:
+    mov al, [peg0_1]
+    jmp hanoi_top_done
+
+hanoi_top_p1:
+    mov al, [peg1_count]
+    cmp al, 0
+    je hanoi_top_empty
+    cmp al, 1
+    je hanoi_top_p1_0
+    cmp al, 2
+    je hanoi_top_p1_1
+    mov al, [peg1_2]
+    jmp hanoi_top_done
+hanoi_top_p1_0:
+    mov al, [peg1_0]
+    jmp hanoi_top_done
+hanoi_top_p1_1:
+    mov al, [peg1_1]
+    jmp hanoi_top_done
+
+hanoi_top_empty:
+    mov al, 0
+hanoi_top_done:
+    pop bl
+    ret
+
+; In AL=peg idx, Out AL=popped disk (0 if empty)
+hanoi_pop:
+    push bl
+    mov bl, al
+
+    cmp bl, 0
+    je hanoi_pop_p0
+    cmp bl, 1
+    je hanoi_pop_p1
+    ; p2
+    mov al, [peg2_count]
+    cmp al, 0
+    je hanoi_pop_empty
+    cmp al, 1
+    je hanoi_pop_p2_0
+    cmp al, 2
+    je hanoi_pop_p2_1
+    mov al, [peg2_2]
+    mov [peg2_2], 0
+    dec [peg2_count]
+    jmp hanoi_pop_done
+hanoi_pop_p2_0:
+    mov al, [peg2_0]
+    mov [peg2_0], 0
+    dec [peg2_count]
+    jmp hanoi_pop_done
+hanoi_pop_p2_1:
+    mov al, [peg2_1]
+    mov [peg2_1], 0
+    dec [peg2_count]
+    jmp hanoi_pop_done
+
+hanoi_pop_p0:
+    mov al, [peg0_count]
+    cmp al, 0
+    je hanoi_pop_empty
+    cmp al, 1
+    je hanoi_pop_p0_0
+    cmp al, 2
+    je hanoi_pop_p0_1
+    mov al, [peg0_2]
+    mov [peg0_2], 0
+    dec [peg0_count]
+    jmp hanoi_pop_done
+hanoi_pop_p0_0:
+    mov al, [peg0_0]
+    mov [peg0_0], 0
+    dec [peg0_count]
+    jmp hanoi_pop_done
+hanoi_pop_p0_1:
+    mov al, [peg0_1]
+    mov [peg0_1], 0
+    dec [peg0_count]
+    jmp hanoi_pop_done
+
+hanoi_pop_p1:
+    mov al, [peg1_count]
+    cmp al, 0
+    je hanoi_pop_empty
+    cmp al, 1
+    je hanoi_pop_p1_0
+    cmp al, 2
+    je hanoi_pop_p1_1
+    mov al, [peg1_2]
+    mov [peg1_2], 0
+    dec [peg1_count]
+    jmp hanoi_pop_done
+hanoi_pop_p1_0:
+    mov al, [peg1_0]
+    mov [peg1_0], 0
+    dec [peg1_count]
+    jmp hanoi_pop_done
+hanoi_pop_p1_1:
+    mov al, [peg1_1]
+    mov [peg1_1], 0
+    dec [peg1_count]
+    jmp hanoi_pop_done
+
+hanoi_pop_empty:
+    mov al, 0
+hanoi_pop_done:
+    pop bl
+    ret
+
+; In AL=peg idx, FL=disk
+hanoi_push:
+    push bl
+    mov bl, al
+
+    cmp bl, 0
+    je hanoi_push_p0
+    cmp bl, 1
+    je hanoi_push_p1
+    ; p2
+    mov al, [peg2_count]
+    cmp al, 0
+    je hanoi_push_p2_0
+    cmp al, 1
+    je hanoi_push_p2_1
+    mov [peg2_2], fl
+    inc [peg2_count]
+    jmp hanoi_push_done
+hanoi_push_p2_0:
+    mov [peg2_0], fl
+    inc [peg2_count]
+    jmp hanoi_push_done
+hanoi_push_p2_1:
+    mov [peg2_1], fl
+    inc [peg2_count]
+    jmp hanoi_push_done
+
+hanoi_push_p0:
+    mov al, [peg0_count]
+    cmp al, 0
+    je hanoi_push_p0_0
+    cmp al, 1
+    je hanoi_push_p0_1
+    mov [peg0_2], fl
+    inc [peg0_count]
+    jmp hanoi_push_done
+hanoi_push_p0_0:
+    mov [peg0_0], fl
+    inc [peg0_count]
+    jmp hanoi_push_done
+hanoi_push_p0_1:
+    mov [peg0_1], fl
+    inc [peg0_count]
+    jmp hanoi_push_done
+
+hanoi_push_p1:
+    mov al, [peg1_count]
+    cmp al, 0
+    je hanoi_push_p1_0
+    cmp al, 1
+    je hanoi_push_p1_1
+    mov [peg1_2], fl
+    inc [peg1_count]
+    jmp hanoi_push_done
+hanoi_push_p1_0:
+    mov [peg1_0], fl
+    inc [peg1_count]
+    jmp hanoi_push_done
+hanoi_push_p1_1:
+    mov [peg1_1], fl
+    inc [peg1_count]
+
+hanoi_push_done:
+    pop bl
+    ret
+
+hanoi_render:
+    push al
+    push bl
+    push cl
+    push dl
+    push el
+    push fl
+
+    call hanoi_clear_screen
+    call hanoi_draw_pegs
+    call hanoi_draw_all_disks
+
+    ; win marker 2x2
+    mov al, [win_flag]
+    cmp al, 1
+    jne hanoi_render_done
+    mov al, COL_WIN
+    mov fl, 0
+    mov el, 0
+    call screen_set_pixel
+    mov fl, 1
+    mov el, 0
+    call screen_set_pixel
+    mov fl, 0
+    mov el, 1
+    call screen_set_pixel
+    mov fl, 1
+    mov el, 1
+    call screen_set_pixel
+
+hanoi_render_done:
+    pop fl
+    pop el
+    pop dl
+    pop cl
+    pop bl
+    pop al
+    ret
+
+hanoi_clear_screen:
+    push al
+    push fl
+    push el
+    mov al, COL_BG
+    mov el, 0
+hanoi_clear_y:
+    cmp el, 32
+    je hanoi_clear_done
+    mov fl, 0
+hanoi_clear_x:
+    cmp fl, 32
+    je hanoi_clear_next_y
+    call screen_set_pixel
+    inc fl
+    jmp hanoi_clear_x
+hanoi_clear_next_y:
+    inc el
+    jmp hanoi_clear_y
+hanoi_clear_done:
+    pop el
+    pop fl
+    pop al
+    ret
+
+hanoi_draw_pegs:
+    push al
+    push fl
+    push el
+
+    mov al, COL_PEG
+
+    ; peg x = 8
+    mov fl, 8
+    mov el, 12
+hanoi_peg0_loop:
+    cmp el, 29
+    je hanoi_peg1_start
+    call screen_set_pixel
+    inc el
+    jmp hanoi_peg0_loop
+
+hanoi_peg1_start:
+    mov fl, 16
+    mov el, 12
+hanoi_peg1_loop:
+    cmp el, 29
+    je hanoi_peg2_start
+    call screen_set_pixel
+    inc el
+    jmp hanoi_peg1_loop
+
+hanoi_peg2_start:
+    mov fl, 24
+    mov el, 12
+hanoi_peg2_loop:
+    cmp el, 29
+    je hanoi_pegs_done
+    call screen_set_pixel
+    inc el
+    jmp hanoi_peg2_loop
+
+hanoi_pegs_done:
+    pop el
+    pop fl
+    pop al
+    ret
+
+hanoi_draw_all_disks:
+    ; draw peg0 slots 0..2, then peg1, peg2
+    call hanoi_draw_peg0
+    call hanoi_draw_peg1
+    call hanoi_draw_peg2
+    ret
+
+hanoi_draw_peg0:
+    mov al, [peg0_0]
+    mov bl, 0
+    mov cl, 0
+    call hanoi_draw_disk_slot
+    mov al, [peg0_1]
+    mov bl, 0
+    mov cl, 1
+    call hanoi_draw_disk_slot
+    mov al, [peg0_2]
+    mov bl, 0
+    mov cl, 2
+    call hanoi_draw_disk_slot
+    ret
+
+hanoi_draw_peg1:
+    mov al, [peg1_0]
+    mov bl, 1
+    mov cl, 0
+    call hanoi_draw_disk_slot
+    mov al, [peg1_1]
+    mov bl, 1
+    mov cl, 1
+    call hanoi_draw_disk_slot
+    mov al, [peg1_2]
+    mov bl, 1
+    mov cl, 2
+    call hanoi_draw_disk_slot
+    ret
+
+hanoi_draw_peg2:
+    mov al, [peg2_0]
+    mov bl, 2
+    mov cl, 0
+    call hanoi_draw_disk_slot
+    mov al, [peg2_1]
+    mov bl, 2
+    mov cl, 1
+    call hanoi_draw_disk_slot
+    mov al, [peg2_2]
+    mov bl, 2
+    mov cl, 2
+    call hanoi_draw_disk_slot
+    ret
+
+; In: AL=disk size(0..3), BL=peg idx(0..2), CL=slot(0 bottom..2 top)
+hanoi_draw_disk_slot:
+    push dl
+    push el
+    push fl
+
+    cmp al, 0
+    je hanoi_dd_done
+
+    ; color by disk size
+    cmp al, 1
+    je hanoi_dd_c1
+    cmp al, 2
+    je hanoi_dd_c2
+    mov al, COL_D3
+    jmp hanoi_dd_color_ok
+hanoi_dd_c1:
+    mov al, COL_D1
+    jmp hanoi_dd_color_ok
+hanoi_dd_c2:
+    mov al, COL_D2
+
+hanoi_dd_color_ok:
+    ; center x by peg
+    cmp bl, 0
+    je hanoi_dd_x0
+    cmp bl, 1
+    je hanoi_dd_x1
+    mov fl, 24
+    jmp hanoi_dd_xok
+hanoi_dd_x0:
+    mov fl, 8
+    jmp hanoi_dd_xok
+hanoi_dd_x1:
+    mov fl, 16
+
+hanoi_dd_xok:
+    ; y by slot: 27,24,21
+    mov el, 27
+    cmp cl, 0
+    je hanoi_dd_yok
+    cmp cl, 1
+    je hanoi_dd_y1
+    mov el, 21
+    jmp hanoi_dd_yok
+hanoi_dd_y1:
+    mov el, 24
+
+hanoi_dd_yok:
+    ; width by size: 3,5,7
+    ; draw horizontal centered line
+    ; start x = center-1 / -2 / -3
+    cmp al, COL_D1
+    je hanoi_dd_w1
+    cmp al, COL_D2
+    je hanoi_dd_w2
+
+    ; w=7
+    sub fl, 3
+    mov dl, 7
+    jmp hanoi_dd_draw
+hanoi_dd_w2:
+    sub fl, 2
+    mov dl, 5
+    jmp hanoi_dd_draw
+hanoi_dd_w1:
+    sub fl, 1
+    mov dl, 3
+
+hanoi_dd_draw:
+hanoi_dd_loop:
+    cmp dl, 0
+    je hanoi_dd_done
+    call screen_set_pixel
+    inc fl
+    dec dl
+    jmp hanoi_dd_loop
+
+hanoi_dd_done:
+    pop fl
+    pop el
+    pop dl
+    ret
